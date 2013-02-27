6f11efb06e3e1da1e55950a25913e1e04c54ad1f
Megha Gulati <megharastogi1988@gmail.com>
2013-02-27 10:48:39 -0800

    breaking down show method

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -12,7 +12,7 @@ class Stats < ActiveRecord::Base
       end   
   end
 
-  def self.show(field,time_range="week",report_start_date=nil,report_end_date=nil)
+  def find_date_range(time_range)
     end_date = Date.today
     case time_range 
       when 'week'   
@@ -20,23 +20,28 @@ class Stats < ActiveRecord::Base
       when 'month'   
        start_date = end_date - 1.month 
       else
-       start_date = end_date - 1.month 
+       start_date = end_date - 1.week 
     end
     if !report_start_date.nil? && !report_end_date.nil?
       start_date = report_start_date.to_date
       end_date = report_end_date.to_date
     end
+  end  
+
+  def self.show(field,time_range="week",report_start_date=nil,report_end_date=nil)
+    
+    find_date_range(time_range)
+
     @stats = Stats.find(:all,:conditions =>["stat_name=? and stat_date >=? and stat_date <=?",field,start_date,end_date])
     @return_stat = []
-    now = start_date
-    while (now <= end_date) do 
-      today_stats = @stats.select{|c| c if c.stat_date == now }
+    while (start_date <= end_date) do 
+      today_stats = @stats.select{|c| c if c.stat_date == start_date }
       if today_stats.blank?
-        @return_stat << [field,now,0]
+        @return_stat << [field,start_date,0]
       else
         @return_stat << [field,today_stats[0].stat_date,today_stats[0].count]  
       end
-      now = now + 1.day
+      start_date = start_date + 1.day
     end
     return @return_stat
   end 
