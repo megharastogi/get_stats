de25aedea4dd745fc1f36145e77b2ab01746caac
Megha Gulati <megharastogi1988@gmail.com>
2013-02-27 10:21:19 -0800

    updating schema file

--- lib/rails/generators/get_stats/templates/schema.rb
+++
@@ -1,9 +0,0 @@
-ActiveRecord::Schema.define(:version => 0) do
-    create_table :stats, :force => true do |t|
-      t.string :stat_name
-      t.date :stat_date
-      t.integer :count
-      t.timestamps
-    end
-    add_index :stats, [:stat_date, :stat_name], :unique => true 
-end
