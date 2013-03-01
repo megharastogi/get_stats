25655ca19c717099b5c70b8b0da3dfeadf579dbc
Megha Gulati <megharastogi1988@gmail.com>
2013-03-01 14:49:17 -0800

    refactoring monthly and weekly methods

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -12,40 +12,16 @@ class Stats < ActiveRecord::Base
       end   
   end
 
-  def self.process_stats(method_type, field, stats, start_date,end_date)
+  def self.process_daily_stats(field, stats, start_date,end_date)
     @return_stat = []
-    stat_collection = 0
-    original_start_date = start_date
     while (start_date <= end_date) do 
       today_stats = @stats.select{|c| c if c.stat_date == start_date }
       if today_stats.blank?
-        @return_stat << [field,start_date,0] if method_type == "daily"
+        @return_stat << [field,start_date,0]
       else
-        @return_stat << [field,today_stats[0].stat_date,today_stats[0].count] if method_type == "daily"
-        stat_collection = stat_collection + today_stats[0].count if method_type == "weekly" || method_type == "monthly"
+        @return_stat << [field,today_stats[0].stat_date,today_stats[0].count]
       end
-
-      if method_type == "weekly"
-        if start_date.monday? && start_date != original_start_date
-        @return_stat << [field,start_date - 1.week ,stat_collection]
-        stat_collection = 0
-        end 
-
-        if start_date == end_date && !start_date.monday?
-           @return_stat << [field,start_date - start_date.wday + 1,stat_collection]
-        end 
-      end  
-
-      if method_type == "monthly"
-        if start_date.mday == 1 && start_date != original_start_date
-         @return_stat << [field,start_date - 1.month ,stat_collection]
-         stat_collection = 0
-        end  
-        if start_date + 1.day == end_date && (start_date + 1.day).mday != 1
-         @return_stat << [field,start_date  ,stat_collection]
-        end 
-      end  
-      start_date = start_date + 1.day
+      start_date = start_date + 1
     end
     return @return_stat
   end  
@@ -64,14 +40,13 @@ class Stats < ActiveRecord::Base
   end  
 
  def self.show(field,time_range="week",report_start_date=nil,report_end_date=nil)
-    
     start_date,end_date = find_date_range(time_range)
     if !report_start_date.nil? && !report_end_date.nil?
       start_date = report_start_date.to_date
       end_date = report_end_date.to_date
     end
     @stats = Stats.find(:all,:conditions =>["stat_name=? and stat_date >=? and stat_date <=?",field,start_date,end_date])
-    process_stats('daily',field, @stats,start_date,end_date)
+    process_daily_stats(field, @stats,start_date,end_date)
   end 
 
   class << self 
@@ -79,31 +54,49 @@ class Stats < ActiveRecord::Base
   end
 
   def self.weekly_stats(field,report_start_date=nil,report_end_date=nil)
-    if report_end_date.nil?
-      report_end_date = Date.today
-    end
-     
-    if report_start_date.nil? || report_start_date >= report_end_date
-      report_start_date = report_end_date - 1.month 
-    end  
-    report_start_date = report_start_date - report_start_date.wday + 1
-    @stats = Stats.find(:all,:conditions =>["stat_name=? and stat_date >=? and stat_date <=?",field,report_start_date,report_end_date])
-    process_stats('weekly',field,@stats,report_start_date,report_end_date)
+    start_date,end_date =  process_dates('weekly',report_start_date,report_end_date)
+    process_weekly_and_monthly_stats('weekly',field,start_date,end_date)
   end
    
-   def self.monthly_stats(field,report_start_date=nil,report_end_date=nil)
-    if report_end_date.nil?
-      report_end_date = Date.today
-    end
-     
-    if report_start_date.nil? || report_start_date >= report_end_date
-      report_start_date = report_end_date - 4.months
-    end  
-    report_start_date = report_start_date - report_start_date.mday + 1
-    @stats = Stats.find(:all,:conditions =>["stat_name=? and stat_date >=? and stat_date <=?",field,report_start_date,report_end_date])
-    process_stats('monthly',field,@stats,report_start_date,report_end_date)
+  def self.monthly_stats(field,report_start_date=nil,report_end_date=nil)
+    start_date,end_date = process_dates('monthly',report_start_date,report_end_date)
+    process_weekly_and_monthly_stats('monthly',field,start_date,end_date)
   end
    
+  def self.process_weekly_and_monthly_stats(method_type,field,start_date,end_date)
+    @stats = Stats.find(:all,:conditions =>["stat_name=? and stat_date >=? and stat_date <=?",field,start_date,end_date])
+    @return_stat = []
+    stat_collection = 0
+    original_start_date = start_date
+    while (start_date <= end_date) do 
+      today_stats = @stats.select{|c| c if c.stat_date == start_date }
+      stat_collection = stat_collection + today_stats[0].count if !today_stats.blank?
+      conitions_day = (method_type == "weekly" ? start_date.monday? : (start_date.mday == 1) )
+      if conitions_day && start_date != original_start_date
+        @return_stat << [field,(method_type == "weekly" ? start_date - 1.week : start_date - 1.month ),stat_collection]
+        stat_collection = 0
+      end 
+      if start_date + 1.day == end_date && !conitions_day
+           @return_stat << [field,start_date,stat_collection]
+      end 
+      start_date = start_date + 1.day
+    end  
+    return @return_stat
+  end 
+
+  def self.process_dates(method_type,start_date,end_date)
+    end_date = Date.today if end_date.nil?
+    if start_date.nil? || start_date >= end_date
+      start_date = end_date - 4.months if method_type == "monthly"
+      start_date = end_date - 1.month if method_type == "weekly"
+    end  
+    if method_type == "weekly"
+      start_date = start_date - start_date.wday + 1
+    else
+      start_date = start_date - start_date.mday + 1
+    end  
+    return start_date, end_date
+  end 
 
   def self.show_all_stat_names
     @stats = Stats.find(:all,:group => 'stat_name')
